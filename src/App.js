f75e4849f003a816205f6c5640bc57b558745d3f
Sharukh <sharukhsm@gmail.com>
2022-03-12 10:36:29 +0530

    Complete Links

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path="/about" element={<About />}></Route>
+        <Route path="about" element={<About />}></Route>
       </Routes>
     </>
   );

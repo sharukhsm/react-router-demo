e18b8ab64d01f3fc93b766159e59cbcb9da1ae75
Sharukh <sharukhsm@gmail.com>
2022-03-14 13:07:51 +0530

    Complete Navigating Programmatically

--- src/components/OrderSummary.js
+++ src/components/OrderSummary.js
@@ -1,5 +1,12 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export const OrderSummary = () => {
-  return <div>Your order is confirmed.</div>;
+  const navigate = useNavigate();
+  return (
+    <>
+      <div>Your order is confirmed.</div>
+      <button onClick={() => navigate(-1)}>Go back</button>
+    </>
+  );
 };

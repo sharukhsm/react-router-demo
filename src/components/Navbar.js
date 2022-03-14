29d60e77f85746914eb6bf8698c5428ed68985c8
Sharukh <sharukhsm@gmail.com>
2022-03-14 13:37:32 +0530

    initial Nested Routes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,6 +17,9 @@ export const Navbar = () => {
       <NavLink to="/about" style={navLinkStyles}>
         About
       </NavLink>
+      <NavLink to="/products" style={navLinkStyles}>
+        Products
+      </NavLink>
     </nav>
   );
 };

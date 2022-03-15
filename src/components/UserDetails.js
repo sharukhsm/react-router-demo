2a577ab0828aefb65fbdefc7d426ff89a508cfce
Sharukh <sharukhsm@gmail.com>
2022-03-15 22:01:06 +0530

    URL params complete

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -1,5 +1,11 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 export const UserDetails = () => {
-  return <div>Details about user.</div>;
+  // const params = useParams();
+  // const userId = params.userId;
+  //or destructured way.
+  const { userId } = useParams();
+
+  return <div>Details about user {userId}.</div>;
 };
